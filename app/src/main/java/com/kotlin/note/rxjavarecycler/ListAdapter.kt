24ba968fb36b0c5cd53e6981be8bea98baaae260
Yulia <123>
2019-09-05 15:35:32 +0400

    init project

---
+++ app/src/main/java/com/kotlin/note/rxjavarecycler/ListAdapter.kt
@@ -0,0 +1,41 @@
+package com.kotlin.note.rxjavarecycler
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import io.reactivex.Observable
+import io.reactivex.subjects.PublishSubject
+import kotlinx.android.synthetic.main.item_list.view.*
+
+class ListAdapter : RecyclerView.Adapter<ListAdapter.ListHolder>() {
+
+    private val subject = PublishSubject.create<String>()
+
+    private val items = listOf("Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7",
+        "Item 8", "Item 9", "Item 10")
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        ListHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_list, parent, false))
+
+    override fun onBindViewHolder(holder: ListHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+    override fun getItemCount() =  items.size
+
+    val clickEvent: Observable<String> = subject
+
+    inner class ListHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+        init {
+            itemView.setOnClickListener {
+                subject.onNext(items[layoutPosition])
+            }
+        }
+
+        fun bind(text: String) {
+            itemView.tvItemText.text = text
+        }
+    }
+}
---
+++ app/src/main/java/com/kotlin/note/rxjavarecycler/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.kotlin.note.rxjavarecycler
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.widget.Toast
+import io.reactivex.disposables.Disposable
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private var disposable: Disposable? = null
+    private val listAdapter = ListAdapter()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        rvList.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = listAdapter
+        }
+
+        setupItemClick()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        disposable?.dispose()
+    }
+
+    private fun setupItemClick() {
+        disposable = listAdapter.clickEvent.subscribe {
+            Toast.makeText(this, "Clicked on $it", Toast.LENGTH_LONG).show()
+        }
+    }
+}
